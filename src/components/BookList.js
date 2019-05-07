274cbb06ffa65d5480b60ad4b1195328ecdd3c2f
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-07 17:25:45 +0300

    Add query to get book details

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
   displayBooks() {
     const { data } = this.props;
     return data.loading ? (
       <div>Loading books...</div>
     ) : (
-      data.books.map(book => <li key={book.id}>{book.name}</li>)
+      data.books.map(book => (
+        <li
+          key={book.id}
+          onClick={e => {
+            this.setState({ selected: book.id });
+          }}
+        >
+          {book.name}
+        </li>
+      ))
     );
   }
   render() {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
