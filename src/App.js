ae1be5e0de6e793318d05b0b8526a9891b745da1
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-07 13:14:26 +0300

    Add books query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import ApolloClient, { HttpLink, InMemoryCache } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import BookList from "./components/BookList";
+
+// Apollo client setup
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>My Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>My Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
